c4a5e07be97807adbf1ef6200a202693f0c63aed
Doug-github-thing <dougrynar@gmail.com>
2025-09-29 10:02:32 -0400

    added loop labels for synthesis anaylsis

--- filter.cpp
+++ filter.cpp
@@ -14,7 +14,7 @@ void fir_filter(int in[size], int out[size]) {
 	// total filter size start
     Outer:for (int i=0; i<size; ++i) {
         // #pragma HLS UNROLL factor=4
-        // #pragma HLS PIPELINE
+        #pragma HLS PIPELINE
 
         // shifting accumulation points
         SHIFT:for (int k=N_TAPS-1; k>0; --k) {
@@ -26,7 +26,7 @@ void fir_filter(int in[size], int out[size]) {
         shift_reg[0] = in[i];
 
         long acc = 0;
-        ACC:for (int k=0; k<N_TAPS; ++k) {
+        ACC:for (int k=N_TAPS-1; k>0; --k) {
             // #pragma HLS UNROLL factor=4
             // #pragma HLS PIPELINE
             acc += shift_reg[k] * co_eff[k];
